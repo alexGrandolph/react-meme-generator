3445fc27fbfc7da783ee46381645213c3282666f
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-11 13:09:31 -0600

    feat: add styling for form

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,7 +5,7 @@ function Meme() {
     <main>
       <form className="form">
         <input type="text" className="form-input" placeholder="Top Text" />
-        <input type="text" className="form-form-input" placeholder="Bottom Text" />
+        <input type="text" className="form-input" placeholder="Bottom Text" />
         <button className="form-button">Get a New Image</button>
       </form>
     </main>
