b375d46099119175f865e0386840680aeaeb9371
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-11 13:31:15 -0600

    feat: add event handler for onClick to generate a random meme url"

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,21 @@
-
+import memesData from '../data/memeData.json'
 
 function Meme() {
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    console.log(url)
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form-input" placeholder="Top Text" />
         <input type="text" className="form-input" placeholder="Bottom Text" />
-        <button className="form-button">Get a New Image</button>
-      </form>
+        <button className="form-button" onClick={getMemeImage}>Get a New Image</button>
+      </div>
     </main>
   )
 }
