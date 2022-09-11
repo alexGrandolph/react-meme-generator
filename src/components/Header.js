205a515b3babb5647e64c13d72836d3a211b53af
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-11 12:31:51 -0600

    feat: add author to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ function Header() {
     <div className="header-container">
       <img className="header-logo" src={Logo} alt="header-logo" />
       <span className="header-text">Meme Generator</span>
+      <span className="header-author">Built by alexGrandolph</span>
     </div>
   )
 }
