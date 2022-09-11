cc74d7dc93d8e33ed163db15d0c27ce51fcb9321
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-11 12:33:37 -0600

    feat: add boiler plate and import for Meme componenet:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './components/Header.js';
+import Meme from './components/Meme.js';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
