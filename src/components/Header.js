1b77c725511ab36d27a34064f98aa8f51f0ae38e
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-11 12:29:09 -0600

    feat: add stlying for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-import Background from '../assets/images/header-background.png';
 import Logo from '../assets/images/header-logo.png';
 
 function Header() {
   return (
     <div className="header-container">
       <img className="header-logo" src={Logo} alt="header-logo" />
-      <img className="header-background" src={Background} alt="header-background" />
       <span className="header-text">Meme Generator</span>
     </div>
   )
